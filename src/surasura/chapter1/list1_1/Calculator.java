07dbd240ff098e1772e8d584b9ac9bca5e18ce4b
NAKAGAKI Kenji <nakaken0629@gmail.com>
2022-04-03 19:59:17 +0900

    list1_1を作成

---
+++ src/surasura/chapter1/list1_1/Calculator.java
@@ -0,0 +1,16 @@
+package surasura.chapter1.list1_1;
+
+/*
+ * Javaプログラムの例です
+ */
+public class Calculator {
+    public static void main(String[] args) {
+        /* 計算をする */
+        int a = 1;
+        int b = 2;
+        int c = a + b;  // cは3になる
+
+        /* 計算した結果を表示する */
+        System.out.println("1 + 2 = " + c);
+    }
+}
